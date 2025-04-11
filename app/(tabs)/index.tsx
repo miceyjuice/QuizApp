7e425eee710a4bcc9a3c6705a3e5f5786c676acc
Paweł Szumiec <pawel.szumiec98@gmail.com>
2025-04-11 23:51:22 +0200

    feat: Enhance WelcomeScreen with new layout, images, and navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,78 +1,87 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import { StyleSheet, Image, TouchableOpacity } from "react-native";
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { Colors } from "@/constants/Colors";
 
-export default function HomeScreen() {
+export default function WelcomeScreen() {
+  const colorScheme = useColorScheme() ?? "light";
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
+    <ThemedView style={styles.container}>
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+
+      <Image
+        source={require("@/assets/images/quiz-app-logo.png")}
+        style={styles.logo}
+      />
+      <ThemedView style={{ alignItems: "center" }}>
         <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
+          source={require("@/assets/images/quiz-welcome-screen-bg.png")}
+          style={styles.img}
         />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: "cmd + d",
-              android: "cmd + m",
-              web: "F12",
-            })}
-          </ThemedText>{" "}
-          to open developer tools.
+
+        <ThemedText type="title" style={styles.title}>
+          Welcome to QuizApp
         </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this
-          starter app.
+
+        <ThemedText style={styles.description}>
+          Test your knowledge with fun quizzes
         </ThemedText>
+
+        <Link
+          href="/quiz"
+          asChild
+          style={{
+            maxWidth: 300, // Replace with an appropriate numeric value or percentage
+          }}
+        >
+          <TouchableOpacity style={styles.startButton}>
+            <ThemedText style={styles.buttonText}>Get started</ThemedText>
+          </TouchableOpacity>
+        </Link>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{" "}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
+  container: {
+    flex: 1,
     alignItems: "center",
-    gap: 8,
+    justifyContent: "center",
+    padding: 20,
+    gap: 40,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  logo: {
+    width: 150,
+    height: 150,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+  img: {
+    width: 300,
+    height: 300,
+  },
+  title: {
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  description: {
+    fontSize: 18,
+    textAlign: "center",
+    marginBottom: 40,
+  },
+  startButton: {
+    backgroundColor: Colors.dark.primaryCtaBtn,
+    paddingVertical: 15,
+    paddingHorizontal: 50,
+    borderRadius: 30,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
